316384408251f6e46b8bd06d1884e8f08d057574
Francisco Mesar <fmesar87@gmail.com>
2022-04-23 20:00:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+ document.querySelector('main').remove()
+ const newHeader = document.createElement('h1')
+ document.body.appendChild(newHeader)
+ newHeader.className = 'victory'
+ newHeader.id = 'victory'
+ newHeader.textContent = 'with "YOUR-NAME is the champion'
+
